b4983b434b53d73a5e568523babef2aa0c4bd2ca
Simeon <stambu@bmlcenter.com>
2025-08-17 20:19:29 +0100

    Fix:send confirm email

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -23,8 +23,11 @@ export class EmailService {
 
     async sendContactEmail(data: EmailDto) {
         const templatePath = join(process.cwd(), 'src', 'email', 'template', 'contact-email.html');
-        //Read the template
+        const templateAutoPath = join(process.cwd(), 'src', 'email', 'template', 'auto-email.html');
+        //Read the templates
         let htmlContent = await readFile(templatePath, 'utf8');
+        let htmlContentAuto = await readFile(templateAutoPath, 'utf8');
+
         htmlContent = htmlContent
             .replace('{{organisation}}', data.organisation)
             .replace('{{typeOrganisation}}', data.typeOrganisation)
@@ -32,14 +35,25 @@ export class EmailService {
             .replace('{{email}}', data.email)
             .replace('{{telephone}}', data.telephone)
             .replace('{{typePartnership}}', data.typePartnership);
+
+        htmlContentAuto = htmlContentAuto
+            .replace('{{name}}', data.name)
+
+        //Send email
         const emailInfo = await this.transporter.sendMail({
             from: `"Le congo que je veux" <${process.env.SMTP_USER}>`,
             to: process.env.RECEIVER_EMAIL,
             subject: 'Nouvelle demande de partenariat',
             html: htmlContent,
         });
-        await this.sheetsService.appendEmail({ ...data, idMail: emailInfo.messageId });
 
+        await this.transporter.sendMail({
+            from: `"Le Congo que je veux" <${process.env.SMTP_USER}>`,
+            to: data.email,
+            subject: 'Confirmation de votre demande de partenariat',
+            html: htmlContentAuto,
+        });
+        await this.sheetsService.appendEmail({ ...data, idMail: emailInfo.messageId });
         return emailInfo;
 
     }
